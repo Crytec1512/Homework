62f1b38d50d024c521b3c01125fb10b1d586378e
339616 <mitrofan1512@gmail.com>
2021-10-20 18:57:57 +0300

    second task

---
+++ Second.py
@@ -0,0 +1,41 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x1
+        self.y = y1
+    def __copy__(self):
+        return self.point2
+    def distance(self, other):
+        return
+
+point1 = Point(x1,y1)
+point2 = Point(x1,y1)
+x1, y1, x2, y2 = [int(x) for x in input().split()]
+point3 = Point(x2,y2)
+
+
+
+
+
+
+
+
+class Triangle:
+    def __init__(self, a, b ,c):
+
+    def perimeter(self):
+
+    def area(self):
+
+    def __contains__(self, point):
+        Triangle.contains(point)
+        point in Triangle
+class Circle:
+    def __init__(self, centre, radius):
+
+    def perimeter(self):
+
+    def area(self):
+
+    def __contains__(self, ):
+
+    def intersects(self):
