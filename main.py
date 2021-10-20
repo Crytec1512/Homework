9c781d69cc809a647fd5a0be0e8d54d0b6c1b45c
Дмитрий Малахов <339616@itip.loc>
2021-10-20 17:57:55 +0300

    First Task

---
+++ main.py
@@ -0,0 +1,24 @@
+class Driver:
+    def __init__(self, name, laps):
+        self.name = name
+        self.laps = laps
+        self.total = sum(laps)
+
+    def __lt__(self, other):
+        return self.total < other.total
+
+array = []
+n, m = [int(x) for x in input().split()]
+for i in range(n):
+    name = input()
+    time = [int(x) for x in input().split()]
+    array.append(Driver(name, time))
+
+
+print(min(array).name)
+
+
+
+
+
+
